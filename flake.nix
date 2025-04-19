294f088c4dbbac9f443b6580bc090b5df130d8a5
NotAShelf <raf@notashelf.dev>
2025-04-19 16:15:40 +0300

    update license

--- flake.nix
+++ flake.nix
@@ -2,10 +2,53 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
 
   outputs = {nixpkgs, ...}: let
-    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux"];
+    inherit (nixpkgs) legacyPackages lib;
+    forAllSystems = lib.genAttrs ["x86_64-linux"];
   in {
+    packages = forAllSystems (system: let
+      pkgs = legacyPackages.${system};
+      fs = lib.fileset;
+    in {
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = "rns";
+        version = "0.1.1";
+        src = fs.toSource {
+          root = ./.;
+          fileset = fs.unions [
+            (fs.fileFilter (file: builtins.any file.hasExt ["rs"]) ./.)
+            ./Cargo.toml
+            ./Cargo.lock
+          ];
+        };
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+
+        buildInputs = [
+          pkgs.luajit
+        ];
+
+        postInstall = ''
+          mkdir -p $out/include
+          install -m755 -D ${./include/rns.h} $out/include/rns.h
+        '';
+
+        meta = {
+          description = "Neovim configuration in Rust, C, or Zig";
+          homepage = "https://github.com/NotAShelf/rns";
+          license = lib.licenses.mpl20;
+          maintainers = [lib.maintainers.NotAShelf];
+        };
+      };
+    });
+
     devShells = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = legacyPackages.${system};
     in {
       default = pkgs.mkShell {
         name = "rns-dev";
